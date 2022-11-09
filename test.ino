58f1f1e22e6445eb54f979a4129c01b69987e01e
jonatan <jonatan.haaralaorosco@gmail.com>
2022-11-09 15:04:14 -0300

    finished

--- test.ino
+++ test.ino
@@ -7,22 +7,16 @@ MD4 = 2;
 MD3 = 2;
 MD2 = 2;
 MD1 = 2;
+
 void setup(){
     pinMode(MD1, INPUT);
-
-}
     pinMode(MD2, INPUT);
     pinMode(MD3, INPUT);
     pinMode(MD4, OUTPUT);
+}
+
 loop setup(){
     avanzar();
     giro_izquierda();
     parar();
-
-}
-loop setup(){
-
-}
-loop setup(){
-
 }
